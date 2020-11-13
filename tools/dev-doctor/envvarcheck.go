6e1fe3040f87dc4907dff32cf985653190819d40
Tom Payne <tom@isovalent.com>
2020-11-13 11:40:12 +0100

    dev-doctor: Add --backporting flag for backporters
    Signed-off-by: Tom Payne <tom@isovalent.com>

---
+++ tools/dev-doctor/envvarcheck.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// An envVarCheck checks that an environment variable is set and not empty.
+type envVarCheck struct {
+	name            string
+	ifNotSetOrEmpty checkResult
+}
+
+func (c *envVarCheck) Name() string {
+	name := c.name
+	name = strings.ToLower(name)
+	name = strings.Replace(name, "_", "-", -1)
+	return name
+}
+
+func (c *envVarCheck) Run() (checkResult, string) {
+	if os.Getenv(c.name) == "" {
+		return c.ifNotSetOrEmpty, fmt.Sprintf("$%s not set or empty", c.name)
+	}
+	return checkOK, fmt.Sprintf("$%s is set", c.name)
+}
+
+func (c *envVarCheck) Hint() string {
+	return ""
+}
